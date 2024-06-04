2ca878ac5307699c54da3f804442daf6d8f44cfe
htetwaiyumdat <50975834+htetwaiyumdat@users.noreply.github.com>
2024-06-04 10:32:11 +0630

    Add files via upload

---
+++ bssapp/conf/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+$server = "10.1.9.190";
+$username = "zabbix";
+$password = "zabbix";
+$db = "zabbix";
+$exec_module="/usr/bin/jobarg_exec";
+$get_module="/usr/bin/jobarg_get";
+$api_url = "http://" . $server . "/zabbix/api_jsonrpc.php";
+?>
